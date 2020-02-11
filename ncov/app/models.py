c4a393a578a0de885445830a136a8415f0bc0417
VXenomac <vxenomac@gmail.com>
2020-02-11 22:31:43 +0800

    Add Files

---
+++ ncov/app/models.py
@@ -0,0 +1,135 @@
+# This is an auto-generated Django model module.
+# You'll have to do the following manually to clean this up:
+#   * Rearrange models' order
+#   * Make sure each model has one field with primary_key=True
+#   * Make sure each ForeignKey and OneToOneField has `on_delete` set to the desired behavior
+#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
+# Feel free to rename the models, but don't rename db_table values or field names.
+from django.db import models
+
+
+class AuthGroup(models.Model):
+    name = models.CharField(unique=True, max_length=150)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_group'
+
+
+class AuthGroupPermissions(models.Model):
+    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
+    permission = models.ForeignKey('AuthPermission', models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_group_permissions'
+        unique_together = (('group', 'permission'),)
+
+
+class AuthPermission(models.Model):
+    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING)
+    codename = models.CharField(max_length=100)
+    name = models.CharField(max_length=255)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_permission'
+        unique_together = (('content_type', 'codename'),)
+
+
+class AuthUser(models.Model):
+    password = models.CharField(max_length=128)
+    last_login = models.DateTimeField(blank=True, null=True)
+    is_superuser = models.BooleanField()
+    username = models.CharField(unique=True, max_length=150)
+    first_name = models.CharField(max_length=30)
+    email = models.CharField(max_length=254)
+    is_staff = models.BooleanField()
+    is_active = models.BooleanField()
+    date_joined = models.DateTimeField()
+    last_name = models.CharField(max_length=150)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user'
+
+
+class AuthUserGroups(models.Model):
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user_groups'
+        unique_together = (('user', 'group'),)
+
+
+class AuthUserUserPermissions(models.Model):
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+    permission = models.ForeignKey(AuthPermission, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user_user_permissions'
+        unique_together = (('user', 'permission'),)
+
+
+class DjangoAdminLog(models.Model):
+    action_time = models.DateTimeField()
+    object_id = models.TextField(blank=True, null=True)
+    object_repr = models.CharField(max_length=200)
+    change_message = models.TextField()
+    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING, blank=True, null=True)
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+    action_flag = models.PositiveSmallIntegerField()
+
+    class Meta:
+        managed = False
+        db_table = 'django_admin_log'
+
+
+class DjangoContentType(models.Model):
+    app_label = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+
+    class Meta:
+        managed = False
+        db_table = 'django_content_type'
+        unique_together = (('app_label', 'model'),)
+
+
+class DjangoMigrations(models.Model):
+    app = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    applied = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'django_migrations'
+
+
+class DjangoSession(models.Model):
+    session_key = models.CharField(primary_key=True, max_length=40)
+    session_data = models.TextField()
+    expire_date = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'django_session'
+
+
+class Ncov(models.Model):
+    confirmedcount = models.IntegerField(db_column='confirmedCount', blank=True, null=True)  # Field name made lowercase.
+    suspectedcount = models.IntegerField(db_column='suspectedCount', blank=True, null=True)  # Field name made lowercase.
+    curedcount = models.IntegerField(db_column='curedCount', blank=True, null=True)  # Field name made lowercase.
+    deadcount = models.IntegerField(db_column='deadCount', blank=True, null=True)  # Field name made lowercase.
+    seriouscount = models.IntegerField(db_column='seriousCount', blank=True, null=True)  # Field name made lowercase.
+    suspectedincr = models.IntegerField(db_column='suspectedIncr', blank=True, null=True)  # Field name made lowercase.
+    confirmedincr = models.IntegerField(db_column='confirmedIncr', blank=True, null=True)  # Field name made lowercase.
+    curedincr = models.IntegerField(db_column='curedIncr', blank=True, null=True)  # Field name made lowercase.
+    deadincr = models.IntegerField(db_column='deadIncr', blank=True, null=True)  # Field name made lowercase.
+    seriousincr = models.IntegerField(db_column='seriousIncr', blank=True, null=True)  # Field name made lowercase.
+
+    class Meta:
+        managed = False
+        db_table = 'ncov'
---
+++ ncov/app/views.py
@@ -0,0 +1,105 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+from app.tools import strtime_to_timestamp_10, timestamp_to_strtime
+from apscheduler.scheduler import Scheduler
+import json, requests
+
+# Create your views here.
+
+sched = Scheduler()
+
+
+@sched.interval_schedule(hours=1)
+def fetch():
+    url = 'https://lab.isaaclin.cn/nCoV/api/overall?latest=0'
+    content = requests.get(url)
+    content = json.loads(content.text)
+    data = []
+    for idx, val in enumerate(content['results']):
+        temp = {}
+        temp['confirmedCount'] = val['confirmedCount']
+        temp['suspectedCount'] = val['suspectedCount']
+        temp['curedCount'] = val['curedCount']
+        temp['deadCount'] = val['deadCount']
+        temp['updateTime'] = val['updateTime']
+        try:
+            temp['seriousCount'] = val['seriousCount']
+        except:
+            temp['seriousCount'] = 'NULL'
+        try:
+            temp['suspectedIncr'] = val['suspectedIncr']
+        except:
+            temp['suspectedIncr'] = 'NULL'
+        try:
+            temp['confirmedIncr'] = val['confirmedIncr']
+        except:
+            temp['confirmedIncr'] = 'NULL'
+        try:
+            temp['curedIncr'] = val['curedIncr']
+        except:
+            temp['curedIncr'] = 'NULL'
+        try:
+            temp['deadIncr'] = val['deadIncr']
+        except:
+            temp['deadIncr'] = 'NULL'
+        try:
+            temp['seriousIncr'] = val['seriousIncr']
+        except:
+            temp['seriousIncr'] = 'NULL'
+        data.append(temp)
+
+    with open('data.json', 'w') as f:
+        json.dump(data, f)
+
+fetch()
+sched.start()
+
+
+def show(request):
+    date = request.GET.get('date')
+    hour = request.GET.get('hour')
+    local_str_time = date + ' ' + hour + ':00:00'
+    timestamp = strtime_to_timestamp_10(local_str_time)
+    with open('data.json', 'r') as f:
+        content = json.load(f)
+    delta = [abs(timestamp - int(str(i['updateTime'])[:-3])) for i in content]
+    target = content[delta.index(min(delta))] 
+    data = {
+        "confirmed_case": target['confirmedCount'],
+        "suspected_case": target['suspectedCount'],
+        "cured_case": target['curedCount'],
+        "death_case": target['deadCount'],
+        "serious_case": target['seriousCount'],
+        "confirmedIncr": target['confirmedIncr'],
+        "suspectedIncr": target['suspectedIncr'],
+        "curedIncr": target['curedIncr'],
+        "deadIncr": target['deadIncr'],
+        "seriousIncr": target['deadIncr'],
+        "time": timestamp_to_strtime(target['updateTime'])[:-7]
+    }
+    return render(request, 'index.html', data)
+
+
+def api(request):
+    date = request.GET.get('date')
+    hour = request.GET.get('hour')
+    local_str_time = date + ' ' + hour + ':00:00'
+    timestamp = strtime_to_timestamp_10(local_str_time)
+    with open('data.json', 'r') as f:
+        content = json.load(f)
+    delta = [abs(timestamp - int(str(i['updateTime'])[:-3])) for i in content]
+    target = content[delta.index(min(delta))] 
+    data = {
+        "confirmed_case": target['confirmedCount'],
+        "suspected_case": target['suspectedCount'],
+        "cured_case": target['curedCount'],
+        "death_case": target['deadCount'],
+        "serious_case": target['seriousCount'],
+        "confirmedIncr": target['confirmedIncr'],
+        "suspectedIncr": target['suspectedIncr'],
+        "curedIncr": target['curedIncr'],
+        "deadIncr": target['deadIncr'],
+        "seriousIncr": target['deadIncr'],
+        "time": timestamp_to_strtime(target['updateTime'])[:-7]
+    }
+    return JsonResponse(data)
